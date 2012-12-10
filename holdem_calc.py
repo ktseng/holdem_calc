e1bfc5270942c4c57c988f903a8c3953004918d3
Kevin Tseng <kevin.tseng@gmail.com>
2012-12-09 23:13:36 -0800

    Added support for exact probability calculation and moved board generation to holdem_functions

--- holdem_calc.py
+++ holdem_calc.py
@@ -1,50 +1,46 @@
 import time
 import holdem_functions
-
-deck = None
-num_iterations = 100000
-
-
-# Generating boards
-def generate_random_boards():
-    import random
-    random.seed(time.time())
-    for iteration in xrange(num_iterations):
-        yield random.sample(deck, 5)
-
-
-# Generate all possible boards
-def generate_exhaustive_boards():
-    import itertools
-    return itertools.combinations(deck, 5)
+import holdem_argparser
 
 
 # Driver function which parses the command line arguments into hole cards,
 # instantiates data structures to hold the intermediate results of the
-# simulations, performs num_iterations simulations, and prints the results
+# simulations, performs the simulations, and prints the results
 def main():
-    global deck
     # Parse command line arguments into hole cards and create deck
-    hole_cards = holdem_functions.parse_cards()
-    deck = holdem_functions.generate_deck(hole_cards)
+    (hole_cards, num_iterations,
+                    exact, given_board, deck) = holdem_argparser.parse_args()
+    num_players = len(hole_cards)
     # Create results data structures which tracks results of comparisons
     # 1) result_histograms: a list for each player that shows the number of
     #    times each type of poker hand (e.g. flush, straight) was gotten
     # 2) winner_list: number of times each player wins the given round
     # 3) result_list: list of the best possible poker hand for each pair of
     #    hole cards for a given board
-    num_players = len(hole_cards)
     result_list, winner_list = [None] * num_players, [0] * (num_players + 1)
     result_histograms = []
     for player in xrange(num_players):
         result_histograms.append([0] * 10)
-    # Run num_iterations simulations
-    generate_boards = generate_random_boards
-    for board in generate_boards():
+    # Choose whether we're running a Monte Carlo or exhaustive simulation
+    board_length = 0 if given_board == None else len(given_board)
+    # When a board is given, exact calculation is much faster than Monte Carlo
+    # simulation, so default to exact if a board is given
+    if exact or given_board is not None:
+        generate_boards = holdem_functions.generate_exhaustive_boards
+    else:
+        generate_boards = holdem_functions.generate_random_boards
+    # Run simulations
+    for remaining_board in generate_boards(deck, num_iterations, board_length):
+        # Generate a new board
+        if given_board:
+            board = given_board[:]
+            board.extend(remaining_board)
+        else:
+            board = remaining_board
         # Find the best possible poker hand given the created board and the
         # hole cards and save them in the results data structures
-        (suit_histogram, histogram,
-                        max_suit) = holdem_functions.preprocess_board(board)
+        (suit_histogram,
+                histogram, max_suit) = holdem_functions.preprocess_board(board)
         for index, hole_card in enumerate(hole_cards):
             result_list[index] = holdem_functions.detect_hand(hole_card, board,
                                          suit_histogram, histogram, max_suit)
